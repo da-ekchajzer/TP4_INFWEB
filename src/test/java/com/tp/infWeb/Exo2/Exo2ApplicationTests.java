2802bc7a4589f5e8e47a798c712c27c70775e0f0
da-ekchajzer <da.ekchajzer@gmail.com>
2021-04-07 11:01:46 +0200

    init

---
+++ src/test/java/com/tp/infWeb/Exo2/Exo2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tp.infWeb.Exo2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exo2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
